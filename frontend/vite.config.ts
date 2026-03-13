1b913b14e298e6ff53161409a637649e117860e0
DiogoCarvalho-C0D3 <diogongcarvalho@gmail.com>
2026-03-13 16:40:10 +0000

    fix: explicit allowedHosts in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: true
+		host: '0.0.0.0',
+		allowedHosts: ['lume-alb-453447696.eu-central-1.elb.amazonaws.com']
 	}
 });
